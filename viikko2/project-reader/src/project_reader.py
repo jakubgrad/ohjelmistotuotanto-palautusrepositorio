7b089e3804153fe1c1fab79bfc7665047c70a88f
Jakub Grad <jakub.grad@icloud.com>
2024-11-11 16:45:27 +0200

    Project-reader exercise

--- viikko2/project-reader/src/project_reader.py
+++ viikko2/project-reader/src/project_reader.py
@@ -1,4 +1,5 @@
 from urllib import request
+import toml
 from project import Project
 
 
@@ -7,9 +8,22 @@ class ProjectReader:
         self._url = url
 
     def get_project(self):
-        # tiedoston merkkijonomuotoinen sisältö
         content = request.urlopen(self._url).read().decode("utf-8")
-        print(content)
 
-        # deserialisoi TOML-formaatissa oleva merkkijono ja muodosta Project-olio sen tietojen perusteella
-        return Project("Test name", "Test description", [], [])
+        parsed_content = toml.loads(content)
+
+        project_name = parsed_content['tool']['poetry']['name']
+        project_description = parsed_content['tool']['poetry']['description']
+        project_license = parsed_content['tool']['poetry']['license']
+        project_authors = parsed_content['tool']['poetry']['authors']
+        dependencies = parsed_content['tool']['poetry']['dependencies']
+        development_dependencies = parsed_content['tool']['poetry']['group']['dev']['dependencies']
+
+        return Project(
+            project_name,
+            project_description,
+            project_license,
+            project_authors,
+            dependencies,
+            development_dependencies
+        )
