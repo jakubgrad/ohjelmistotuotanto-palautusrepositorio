cf13c93542a3166c55a5a9ea4eb2e7d9eddef5c2
Jakub Grad <jakub.grad@icloud.com>
2024-11-11 19:13:44 +0200

    13. Graphic Player List

--- viikko2/nhl-reader/src/index.py
+++ viikko2/nhl-reader/src/index.py
@@ -1,15 +1,34 @@
-import requests
 from player_reader import PlayerReader
 from player_stats import PlayerStats
+from rich import print
+from rich.table import Table
+from rich.prompt import Prompt
+
+def display_top_scorers(players, nationality, season):
+    table = Table(title=f"Top scorers of {nationality} season {season}")
+    table.add_column("name")
+    table.add_column("team")
+    table.add_column("goals")
+    table.add_column("assists")
+    table.add_column("points")
+
+    for player in players:
+        table.add_row(player.name, player.team, str(player.goals), str(player.assists), str(player.points))
+
+    print(table)
 
 def main():
     url = "https://studies.cs.helsinki.fi/nhlstats/2023-24/players"
     reader = PlayerReader(url)
     stats = PlayerStats(reader)
-    players = stats.top_scorers_by_nationality("FIN")
 
-    for player in players:
-        print(player)
+    season = Prompt.ask("Select season [2018-19/2019-20/2020-21/2021-22/2022-23/2023-24/2024-25]").strip()
+
+    while True:
+        nationality = Prompt.ask("Select nationality [AUT/CZE/AUS/SWE/GER/DEN/SUI/SVK/NOR/RUS/CAN/LAT/BLR/SLO/USA/FIN/GBR]").strip().upper()
+        
+        players = stats.top_scorers_by_nationality(nationality)
+        display_top_scorers(players, nationality, season)
 
 if __name__ == "__main__":
     main()
