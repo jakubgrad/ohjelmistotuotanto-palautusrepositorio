0d5a5c3bc72ea2eaecbfa8871bd0aa0bf492826a
Jakub Grad <jakub.grad@icloud.com>
2024-11-25 15:03:07 +0200

    Aloita_asiointi resets the data of the previous purchase

--- viikko4/verkkokauppa/src/tests/kauppa_test.py
+++ viikko4/verkkokauppa/src/tests/kauppa_test.py
@@ -206,3 +206,39 @@ class TestKauppa(unittest.TestCase):
 
             # varmistetaan, että metodia tilisiirto on kutsuttu with correct arguments
             pankki_mock.tilisiirto.assert_called_with("pekka", 42, "12345", "33333-44455", 5)
+
+    def test_aloita_asiointi_resets_the_data_of_the_previous_purchase(self):
+        pankki_mock = Mock()
+        viitegeneraattori_mock = Mock()
+
+        # palautetaan aina arvo 42
+        viitegeneraattori_mock.uusi.return_value = 42
+
+        varasto_mock = Mock()
+
+        # tehdään toteutus saldo-metodille
+        def varasto_saldo(tuote_id):
+            if tuote_id == 1:
+                return 10
+
+        # tehdään toteutus hae_tuote-metodille
+        def varasto_hae_tuote(tuote_id):
+            if tuote_id == 1:
+                return Tuote(1, "maito", 5)
+
+        # otetaan toteutukset käyttöön
+        varasto_mock.saldo.side_effect = varasto_saldo
+        varasto_mock.hae_tuote.side_effect = varasto_hae_tuote
+
+        # alustetaan kauppa
+        kauppa = Kauppa(varasto_mock, pankki_mock, viitegeneraattori_mock)
+
+        # tehdään ostokset
+        kauppa.aloita_asiointi()
+        kauppa.lisaa_koriin(1)
+        
+        self.assertEqual(kauppa._ostoskori.hinta(), 5)
+        
+        kauppa.aloita_asiointi()
+
+        self.assertEqual(kauppa._ostoskori.hinta(), 0)
