6badaea539d10544631dab7922cbc356cbf8ed40
Jakub Grad <jakub.grad@icloud.com>
2024-11-25 14:54:27 +0200

    Test 4/4

--- viikko4/verkkokauppa/src/tests/kauppa_test.py
+++ viikko4/verkkokauppa/src/tests/kauppa_test.py
@@ -162,3 +162,47 @@ class TestKauppa(unittest.TestCase):
 
         # varmistetaan, että metodia tilisiirto on kutsuttu with correct arguments
         pankki_mock.tilisiirto.assert_called_with("pekka", 42, "12345", "33333-44455", 10)
+
+        #Let's start the transaction, add a product that is enough in stock and a product that
+        #is out of stock to the basket and make the purchase, make sure that the bank's method 
+        #is called tilisiirtowith the correct customer, account number and amount
+    def test_tilisiirto_is_called_correctly_for_product_that_is_missing_and_one_that_is_enough(self):
+            pankki_mock = Mock()
+            viitegeneraattori_mock = Mock()
+
+            # palautetaan aina arvo 42
+            viitegeneraattori_mock.uusi.return_value = 42
+
+            varasto_mock = Mock()
+
+            # tehdään toteutus saldo-metodille
+            def varasto_saldo(tuote_id):
+                if tuote_id == 1:
+                    return 10
+                if tuote_id == 2:
+                    return 0
+
+            # tehdään toteutus hae_tuote-metodille
+            def varasto_hae_tuote(tuote_id):
+                if tuote_id == 1:
+                    return Tuote(1, "maito", 5)
+                if tuote_id == 2:
+                    return Tuote(2, "cream", 3)
+
+            # otetaan toteutukset käyttöön
+            varasto_mock.saldo.side_effect = varasto_saldo
+            varasto_mock.hae_tuote.side_effect = varasto_hae_tuote
+
+            # alustetaan kauppa
+            kauppa = Kauppa(varasto_mock, pankki_mock, viitegeneraattori_mock)
+
+            # tehdään ostokset
+            kauppa.aloita_asiointi()
+            
+            #adding two of the same product
+            kauppa.lisaa_koriin(1)
+            kauppa.lisaa_koriin(2)
+            kauppa.tilimaksu("pekka", "12345")
+
+            # varmistetaan, että metodia tilisiirto on kutsuttu with correct arguments
+            pankki_mock.tilisiirto.assert_called_with("pekka", 42, "12345", "33333-44455", 5)
