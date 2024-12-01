a0154ea50ffb5061fa2cbc1ce1133e4a4e366a5d
Jakub Grad <jakub.grad@icloud.com>
2024-12-01 23:22:50 +0200

    Initial improvemenets

--- viikko5/tennis/src/tennis_game.py
+++ viikko5/tennis/src/tennis_game.py
@@ -1,31 +1,40 @@
+class Player:
+    def __init__(self, name, score=0):
+        self.name = name
+        self.score = score 
+    
+    def increment_score(self):
+        self.score = self.score + 1
+
 class TennisGame:
     def __init__(self, player1_name, player2_name):
-        self.player1_name = player1_name
-        self.player2_name = player2_name
-        self.m_score1 = 0
-        self.m_score2 = 0
+        self.player1 = Player(player1_name)
+        self.player2 = Player(player2_name)
+        self.player1.score = 0
+        self.player2.score = 0
 
     def won_point(self, player_name):
-        if player_name == "player1":
-            self.m_score1 = self.m_score1 + 1
+        if player_name == self.player1.name:
+            self.player1.score = self.player1.score + 1
         else:
-            self.m_score2 = self.m_score2 + 1
+            self.player2.score = self.player2.score + 1
+
 
     def get_score(self):
         score = ""
         temp_score = 0
 
-        if self.m_score1 == self.m_score2:
-            if self.m_score1 == 0:
+        if self.player1.score == self.player2.score:
+            if self.player1.score == 0:
                 score = "Love-All"
-            elif self.m_score1 == 1:
+            elif self.player1.score == 1:
                 score = "Fifteen-All"
-            elif self.m_score1 == 2:
+            elif self.player1.score == 2:
                 score = "Thirty-All"
             else:
                 score = "Deuce"
-        elif self.m_score1 >= 4 or self.m_score2 >= 4:
-            minus_result = self.m_score1 - self. m_score2
+        elif self.player1.score >= 4 or self.player2.score >= 4:
+            minus_result = self.player1.score - self.player2.score
 
             if minus_result == 1:
                 score = "Advantage player1"
@@ -38,10 +47,10 @@ class TennisGame:
         else:
             for i in range(1, 3):
                 if i == 1:
-                    temp_score = self.m_score1
+                    temp_score = self.player1.score
                 else:
                     score = score + "-"
-                    temp_score = self.m_score2
+                    temp_score = self.player2.score
 
                 if temp_score == 0:
                     score = score + "Love"
