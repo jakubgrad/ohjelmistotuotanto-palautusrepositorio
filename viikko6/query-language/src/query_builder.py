b4c936e0dc6c08f80d6fea752806d9b75a7b2605
Jakub Grad <jakub.grad@icloud.com>
2024-12-09 16:34:00 +0200

    4. Improved Query Language, Part 1

--- viikko6/query-language/src/query_builder.py
+++ viikko6/query-language/src/query_builder.py
@@ -1,8 +1,17 @@
-from matchers import All
+from matchers import *
 
 class QueryBuilder:
     def __init__(self, build = All()):
         self.build_object = build
+
+    def plays_in(self,team_name):
+        return QueryBuilder(And(self.build_object, PlaysIn(team_name)))
+
+    def has_at_least(self, number, attribute):
+        return QueryBuilder(And(self.build_object, HasAtLeast(number, attribute)))
+
+    def has_fewer_than(self, number, attribute):
+        return QueryBuilder(And(self.build_object, HasFewerThan(number, attribute)))
 
     def build(self):
         return self.build_object
